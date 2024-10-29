526a86d84f7f90d59bc0922db92377ecab1f9c70
clakr <clarktolosa@gmail.com>
2024-10-29 13:47:50 +0800

    refactor(dashboard): transactions data is now derived

--- app/(authed)/page.tsx
+++ app/(authed)/page.tsx
@@ -16,8 +16,9 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { db } from "@/db";
+import { Bucket } from "@/db/schema";
 import { currencyFormatter, truncateString } from "@/lib/utils";
-import { currentUser } from "@clerk/nextjs/server";
+import { currentUser, User } from "@clerk/nextjs/server";
 import { Plus } from "lucide-react";
 import Link from "next/link";
 import { Suspense } from "react";
@@ -25,66 +26,113 @@ import { Suspense } from "react";
 export default async function Page() {
   return (
     <Main className="grid gap-y-8">
-      <BucketsSection />
-      <TransactionsSection />
+      <Suspense fallback={<p>loading...</p>}>
+        <MainContent />
+      </Suspense>
     </Main>
   );
 }
 
-function BucketsSection() {
-  return (
-    <section className="grid gap-y-2">
-      <h2 className="text-xl font-bold">Buckets</h2>
-      <Suspense fallback={<p>loading...</p>}>
-        <BucketList />
-      </Suspense>
-    </section>
-  );
-}
-
-async function BucketList() {
-  const authedUser = await currentUser();
-  if (!authedUser) return;
-
+async function getData(userId: User["id"]) {
   const buckets = await db.query.buckets.findMany({
     columns: {
       createdAt: false,
       updatedAt: false,
       userId: false,
     },
-    where: (buckets, { eq }) => eq(buckets.userId, authedUser.id),
+    where: (buckets, { eq }) => eq(buckets.userId, userId),
     orderBy: (buckets, { desc }) => [desc(buckets.totalAmount)],
+    with: {
+      transactions: {
+        columns: {
+          bucketId: false,
+          updatedAt: false,
+        },
+      },
+    },
   });
 
+  type PartialTransactionsWithBucketName =
+    ((typeof buckets)[number]["transactions"][number] & {
+      bucket: { name: Bucket["name"] };
+    })[];
+
+  const transactions = buckets
+    .reduce<PartialTransactionsWithBucketName>((previous, current) => {
+      if (current.transactions.length) {
+        previous.push(
+          ...current.transactions.map((transaction) => ({
+            ...transaction,
+            bucket: { name: current.name },
+          })),
+        );
+      }
+
+      return previous;
+    }, [])
+    .toSorted(
+      (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+    );
+
+  return { buckets, transactions };
+}
+
+async function MainContent() {
+  const authedUser = await currentUser();
+  if (!authedUser) return;
+
+  const { buckets, transactions } = await getData(authedUser.id);
+
   return (
-    <div className="grid auto-rows-fr grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">
-      {buckets.map((bucket) => (
-        <Card
-          key={bucket.id}
-          className="relative isolate flex flex-col justify-between"
-        >
-          <BucketDropdownMenu bucketId={bucket.id} />
-          <CardHeader>
-            <CardTitle>{truncateString(bucket.name)}</CardTitle>
-            <CardDescription>
-              {bucket.description ? truncateString(bucket.description) : null}
-            </CardDescription>
-          </CardHeader>
-          <CardFooter className="justify-end">
-            <strong>{currencyFormatter.format(+bucket.totalAmount)}</strong>
-          </CardFooter>
-        </Card>
-      ))}
-      <Link href="/buckets/create">
-        <Card className="grid h-full place-content-center hover:bg-primary/5">
-          <Plus className="size-20" />
-        </Card>
-      </Link>
-    </div>
+    <>
+      <BucketList buckets={buckets} />
+      <TransactionsTable transactions={transactions} />
+    </>
   );
 }
 
-function TransactionsSection() {
+function BucketList({
+  buckets,
+}: {
+  buckets: Awaited<ReturnType<typeof getData>>["buckets"];
+}) {
+  return (
+    <section className="grid gap-y-2">
+      <h2 className="text-xl font-bold">Buckets</h2>
+      <div className="grid auto-rows-fr grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">
+        {buckets.map((bucket) => (
+          <Card
+            key={bucket.id}
+            className="relative isolate flex flex-col justify-between"
+          >
+            <BucketDropdownMenu bucketId={bucket.id} />
+            <CardHeader>
+              <CardTitle>{truncateString(bucket.name)}</CardTitle>
+              <CardDescription>
+                {bucket.description ? truncateString(bucket.description) : null}
+              </CardDescription>
+            </CardHeader>
+            <CardFooter className="justify-end">
+              <strong>{currencyFormatter.format(+bucket.totalAmount)}</strong>
+            </CardFooter>
+          </Card>
+        ))}
+        <Link href="/buckets/create">
+          <Card className="grid h-full place-content-center hover:bg-primary/5">
+            <Plus className="size-20" />
+          </Card>
+        </Link>
+      </div>
+    </section>
+  );
+}
+
+function TransactionsTable({
+  transactions,
+}: {
+  transactions: Awaited<ReturnType<typeof getData>>["transactions"];
+}) {
   return (
     <section className="grid gap-y-2">
       <h2 className="text-xl font-bold">Recent Transactions</h2>
@@ -106,39 +154,32 @@ function TransactionsSection() {
               </TableRow>
             }
           >
-            <TransactionList />
+            {transactions.length > 0 ? (
+              transactions.map((transaction) => (
+                <TableRow key={transaction.id}>
+                  <TableCell>{transaction.id}</TableCell>
+                  <TableCell className="whitespace-nowrap">
+                    {transaction.description}
+                  </TableCell>
+                  <TableCell>{transaction.amount}</TableCell>
+                  <TableCell className="font-medium">
+                    {transaction.bucket.name}
+                  </TableCell>
+                  <TableCell>
+                    {transaction.createdAt.toLocaleString()}
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={4} className="text-center">
+                  No transactions yet.
+                </TableCell>
+              </TableRow>
+            )}
           </Suspense>
         </TableBody>
       </Table>
     </section>
   );
-}
-
-async function TransactionList() {
-  const transactions = await db.query.transactions.findMany({
-    columns: {
-      updatedAt: false,
-    },
-    // @todo: add where clause
-    orderBy: (transactions, { desc }) => [desc(transactions.createdAt)],
-    with: {
-      bucket: {
-        columns: {
-          name: true,
-        },
-      },
-    },
-  });
-
-  return transactions.map((transaction) => (
-    <TableRow key={transaction.id}>
-      <TableCell>{transaction.id}</TableCell>
-      <TableCell className="whitespace-nowrap">
-        {transaction.description}
-      </TableCell>
-      <TableCell>{transaction.amount}</TableCell>
-      <TableCell className="font-medium">{transaction.bucket.name}</TableCell>
-      <TableCell>{transaction.createdAt.toLocaleString()}</TableCell>
-    </TableRow>
-  ));
 }
