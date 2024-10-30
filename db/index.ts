f8d0f4438de1efc06c6c1c6de2576519c0bb135b
clakr <clarktolosa@gmail.com>
2024-10-31 07:23:17 +0800

    chore(app): migrated db providers

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,4 @@
 import * as schema from "./schema";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle } from "drizzle-orm/neon-http";
 
-export const db = drizzle({ schema });
+export const db = drizzle(process.env.DATABASE_URL!, { schema });
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,9 +5,6 @@ export default defineConfig({
   schema: "./db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    host: process.env.POSTGRES_HOST ?? "",
-    user: process.env.POSTGRES_USER ?? "",
-    password: process.env.POSTGRES_PASSWORD ?? "",
-    database: process.env.POSTGRES_DATABASE ?? "",
+    url: process.env.DATABASE_URL!,
   },
 });
